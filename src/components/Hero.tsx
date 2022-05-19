4da389d2c4fa3ea1aa1365b441efc9e05322c5d9
freesudani <mohamed.dahiya@hotmail.com>
2022-05-19 12:02:05 +0700

    Adjusting MQ

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import FashionImage from "../images/Girls-Fashion-Trends_prev_ui.png";
 
 const Hero = () => {
   return (
-    <div className="relative h-screen mb:h-[90rem]  w-screen bg-hero-bg bg-no-repeat bg-center bg-cover">
+    <div className="relative h-screen mb:h-[75rem]  w-screen bg-hero-bg bg-no-repeat bg-center bg-cover">
       <Navbar />
 
       <div className="absolute left-20 top-10 sm:left-10 sm:top-5 mb:left-5">
@@ -15,10 +15,10 @@ const Hero = () => {
         <img src={FashionImage} alt="fashion" className="w-100" />
       </div>
       <div className="ml-36 mt-60 2xl:mt-72 md:ml-48 sm:text-center sm:ml-0">
-        <h1 className="text-9xl sm:text-8xl mb:text-7xl bold uppercase leading-relaxed">
+        <h1 className="text-9xl sm:text-8xl mb:text-7xl mb:text-white bold uppercase leading-relaxed">
           Be Each
           <br /> day{" "}
-          <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-primary-200 relative inline-block">
+          <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 mb:before:-skew-y-5 before:bg-primary-200 relative inline-block">
             <span className="relative text-white">Beautiful</span>
           </span>
         </h1>
--- src/components/Print.tsx
+++ src/components/Print.tsx
@@ -3,13 +3,13 @@ import ShirtPrintImg from "../images/shirtprinting_prev_ui.png";
 
 const Print = () => {
   return (
-    <div className="flex justify-around items-center bg-print-bg h-100 bg-no-repeat bg-cover bg-center px-10 py-5">
-      <div className=" 2xl:mt-72 md:ml-0 sm:text-center sm:ml-0  -translate-y-20">
+    <div className="flex justify-around items-center bg-print-bg h-100  bg-no-repeat bg-cover bg-center px-10 py-5">
+      <div className=" 2xl:mt-72 mb:mt-28 md:ml-0 sm:text-center sm:ml-0  -translate-y-20">
         <h1 className="text-8xl text-white text-left  md:text-7xl sm:text-6xl mb:text-5xl bold uppercase">
           spice up your shirts
           <br /> and Add style{" "}
         </h1>
-        <p className="text-2xl text-white text-left my-11">
+        <p className="text-2xl text-white text-left my-11 mb:my-9">
           use our latest technology and Stand out from the crowd with custom
           T-shirts in a range of designs,
           <br /> colors and styles.
@@ -18,7 +18,7 @@ const Print = () => {
           <button className="secondarybt lgbt">Learn More</button>
         </div>
       </div>
-      <div className="">
+      <div className="mb:-mt-40">
         <img src={ShirtPrintImg} alt="ShirtPrint" className="w-100 h-auto" />
       </div>
     </div>
