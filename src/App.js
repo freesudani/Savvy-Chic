0cf5fb54848d860c0d5e6d421483c22bffd95dca
freesudani <mohamed.dahiya@hotmail.com>
2022-05-20 10:53:32 +0700

    Partnering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero.tsx";
 import Print from "./components/Print.tsx";
+import Partnering from "./components/Partnering.tsx";
 
 const App = () => {
   return (
     <div>
       <Hero />
       <Print />
+      <Partnering />
     </div>
   );
 };
---
+++ src/components/Partnering.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import PartnerLogo from "../images/cecf96b6522f4362aa3b28dbb74c90cc.png";
+
+const Partnering = () => {
+  return (
+    <div className="flex justify-center items-center h-screen lg:h-[80vh]">
+      <div className="flex flex-col justify-evenly h-5/6 2xl:h-4/6 w-9/12 shadow-xl rounded-md bg-partner-bg bg-cover bg-center md:h-full">
+        <div className="flex justify-center items-center h-36">
+          <h2 className="text-white text-7xl md:text-6xl mb:text-center mb:text-5xl">
+            Drops 7th August
+          </h2>
+        </div>
+        <div className="flex justify-around items-center md:flex-col">
+          <div className="w-1/2 md:w-full">
+            <h3 className="text-white text-5xl md:text-center sm:text-4xl mb:text-3xl">
+              <span className="text-primary-200">Savvy Chic</span> is partnering
+              with <span className="text-[#D6BFA9]">Friendly Fashionista </span>
+              to bring to you a limited-time Clothing, Cosmetics and accessories
+              from the{" "}
+              <span className="text-[#D6BFA9]">Friendly Fashionista </span>
+              Target collection bursting with endless joy and variety.
+            </h3>
+          </div>
+          <div className="md:mt-32">
+            <img src={PartnerLogo} alt="Partner Logo" className="rounded-md" />
+          </div>
+        </div>
+        <div className="flex justify-center items-center  ">
+          <button className="primarybt lgbt h-32 w-72 uppercase sm:h-28 sm:w-64">
+            Preview The Collection
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Partnering;
